069981214b5f5398554250fd981999fe6733e556
Sawata Gore <sawata.gore@dtskill.com>
2025-07-25 21:42:13 +0530

    Added new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { MOCK_COUNTRY_OPTIONS } from "./MachineCoding/PhoneBook/Constents";
+import PhoneBook from "./MachineCoding/PhoneBook/PhoneBook";
 
 function App() {
-  return <main>App </main>;
+  return (
+    <main>
+      <PhoneBook options={MOCK_COUNTRY_OPTIONS} />
+    </main>
+  );
 }
 
 export default App;
